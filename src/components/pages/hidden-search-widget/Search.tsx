71ea05b35e184bdfe529c1e1e66a7e514f8cfd34
CJ <cjfritz9@gmail.com>
2023-01-25 17:20:46 -0700

    collapse search bar upon clicking outside of it

--- src/components/pages/hidden-search-widget/Search.tsx
+++ src/components/pages/hidden-search-widget/Search.tsx
@@ -5,26 +5,48 @@ import './Search.css';
 import Title from '../../Title';
 
 const Search: React.FC = () => {
-  const handleClick = () => {
+  const handleClick = (e: React.MouseEvent) => {
     const searchElement = document.getElementById('search')! as HTMLDivElement;
-    const searchInput = document.getElementById('input')! as HTMLInputElement;
-    searchElement.classList.toggle('active');
-    searchInput.focus();
+    const searchInput = document.getElementById(
+      'search-input'
+    )! as HTMLInputElement;
+    const searchIcon = document.getElementById('search-icon')! as HTMLElement;
+    const searchBtn = document.getElementById(
+      'search-btn'
+    )! as HTMLButtonElement;
+    console.log(e.target, searchElement);
+    if (
+      e.target == searchIcon ||
+      e.target == searchBtn ||
+      e.target == searchElement ||
+      e.target == searchInput
+    ) {
+      searchElement.classList.add('active');
+      searchInput.focus();
+    } else {
+      searchElement.classList.remove('active');
+    }
   };
 
   return (
-    <Chakra.Flex bgColor='blue.200' className='component-container'>
+    <Chakra.Flex
+      bgColor='blue.200'
+      className='component-container'
+      onClick={(e: React.MouseEvent) => handleClick(e)}
+    >
       <Title title='Hidden Search Widget' color='white' font='Poppins' />
 
       <div id='search' className='search-container'>
         <input
           type='text'
-          id='input'
+          id='search-input'
           className='search-input'
           placeholder='Search...'
         />
-        <button className='search-btn' onClick={handleClick.bind(this)}>
-          <i className={googleSymbols}>search</i>
+        <button id='search-btn' className='search-btn'>
+          <i id='search-icon' className={googleSymbols}>
+            search
+          </i>
         </button>
       </div>
     </Chakra.Flex>

beed129b0f167f89ae64dc6a2ce5ee86b7879060
CJ <cjfritz9@gmail.com>
2023-01-27 18:40:33 -0700

    revision of last

--- src/components/pages/movie-app/MovieCard.tsx
+++ src/components/pages/movie-app/MovieCard.tsx
@@ -104,26 +104,7 @@ const MovieCard: React.FC<MovieProps> = ({ movie }) => {
                 </Chakra.Flex>
                 <AnimatePresence>
                   {/* {isHovered && ( */}
-                  <Chakra.Text
-                    as={motion.div}
-                    layout
-                    // initial={{
-                    //   opacity: 0,
-                    //   maxHeight: '0px',
-                    //   minHeight: '0%'
-                    // }}
-                    // animate={{
-                    //   opacity: [0, 0.5, 1],
-                    //   minHeight: ['0%', '100%', '100%'],
-                    //   maxHeight: ['0px', '150px', '150px']
-                    // }}
-                    // exit={{
-                    //   opacity: [1, 0.1, 0],
-                    //   minHeight: ['100%', '100%', '0%'],
-                    //   maxHeight: ['150px', '150px', '0px']
-                    // }}
-                    cursor='default'
-                  >
+                  <Chakra.Text as={motion.div} layout cursor='default'>
                     {movie.overview.length >= 288
                       ? trimOverview(movie.overview)
                       : movie.overview}
@@ -145,25 +126,12 @@ const MovieCard: React.FC<MovieProps> = ({ movie }) => {
                   {/* {isHovered && ( */}
                   <Chakra.Button
                     _hover={{ backgroundColor: 'transparent' }}
-                    // as={motion.div}
-                    // initial={{
-                    //   display: 'none'
-                    // }}
-                    // animate={{
-                    //   opacity: [0, 1, 1],
-                    //   display: ['none', 'block', 'block']
-                    // }}
-                    // exit={{
-                    //   opacity: [1, 1, 0],
-                    //   maxHeight: ['100px', '100px', '0px']
-                    // }}
                     cursor='pointer'
                     variant='ghost'
                     colorScheme='blue'
                   >
                     More
                   </Chakra.Button>
-                  {/* )} */}
                 </AnimatePresence>
               </Chakra.ButtonGroup>
             </Chakra.CardFooter>
